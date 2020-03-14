cab03bbbc063181c7a4cc1e2aaeddb9d75efe85f
kenzo0107 <kenzo.tanaka0107@gmail.com>
2020-03-15 00:40:21 +0900

    Delete steps of cross accounts by using assume role
    It is so complex. I want to simplify this project.

--- pkg/awsapi/session.go
+++ pkg/awsapi/session.go
@@ -2,30 +2,14 @@ package awsapi
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sts"
 )
 
 // NewSession : new session
 func NewSession(c *sts.Credentials, region string) *session.Session {
-	var config aws.Config
-	if c != nil {
-		// credentials exist ... except AWS Account which assume role doesn't belong to
-		creds := credentials.NewStaticCredentials(
-			*c.AccessKeyId,
-			*c.SecretAccessKey,
-			*c.SessionToken,
-		)
-		config = aws.Config{
-			Region:      aws.String(region),
-			Credentials: creds,
-		}
-	} else {
-		// only AWS Account which assume role belongs to
-		config = aws.Config{
-			Region: aws.String(region),
-		}
+	config := aws.Config{
+		Region: aws.String(region),
 	}
 	return session.Must(session.NewSession(&config))
 }
--- pkg/awsapi/session_test.go
+++ pkg/awsapi/session_test.go
@@ -1,50 +1,15 @@
 package awsapi
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/service/sts"
-	"github.com/google/go-cmp/cmp"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
 )
 
-func TestNewSession(t *testing.T) {
-	region := "ap-northeast-1"
-	sess := NewSession(&sts.Credentials{
-		AccessKeyId:     aws.String("hogehoge"),
-		SecretAccessKey: aws.String("mogemoge"),
-		SessionToken:    aws.String("barbar"),
-	}, region)
-
-	actual, err := sess.Config.Credentials.Get()
+func TestNewSessionWithoutCredentials(t *testing.T) {
+	sess := NewSession(nil, endpoints.ApNortheast1RegionID)
+	_, err := sess.Config.Credentials.Get()
 	if err != nil {
 		t.Error(err)
-	}
-
-	expected := credentials.Value{
-		AccessKeyID:     "hogehoge",
-		SecretAccessKey: "mogemoge",
-		SessionToken:    "barbar",
-		ProviderName:    "StaticProvider",
-	}
-
-	if diff := cmp.Diff(expected, actual); diff != "" {
-		t.Errorf("wrong result : %s", diff)
-	}
-}
-
-func TestNewSessionWithoutCredentials(t *testing.T) {
-	credentialsPath := filepath.Join("..", "..", "testdata", "credentials")
-	if err := os.Setenv("AWS_SHARED_CREDENTIALS_FILE", credentialsPath); err != nil {
-		t.Error("error occured in os.Setenv(\"AWS_SHARED_CREDENTIALS_FILE\")")
-	}
-	region := "ap-northeast-1"
-	sess := NewSession(nil, region)
-	_, err := sess.Config.Credentials.Get()
-	if err == nil {
-		t.Errorf("wrong result : err is nil. \n%s", err.Error())
 	}
 }
